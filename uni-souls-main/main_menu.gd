e7daa56ffd5f3ac1329aefa3ff2ef8f53889c694
Guilherme Mitsuo Honda <guihonda10@gmail.com>
2025-10-05 14:32:59 -0300

    Criação do menu Options, com volume da música e atalho para fullscreen

--- uni-souls-main/main_menu.gd
+++ uni-souls-main/main_menu.gd
@@ -1,14 +1,15 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _ready():
+	main_buttons.visible = true
+	options.visible = false
 
 
 func _on_start_pressed() -> void:
@@ -16,8 +17,14 @@ func _on_start_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	print("Options pressed")
+	main_buttons.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
